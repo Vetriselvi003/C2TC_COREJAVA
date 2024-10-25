a0156aa4f2bdeebcc711cacb7792bdc3baf0ac39
LENOVO <LENOVO@-RB-27>
2024-10-25 08:33:19 +0530

    First Commit

---
+++ C2TC_VETRISELVI/src/com/tns/ifet/dayone/FirstProgram.java
@@ -0,0 +1,11 @@
+package com.tns.ifet.dayone;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Vetriselvi Thirumaran");
+		// TODO Auto-generated method stub
+
+	}
+
+}
