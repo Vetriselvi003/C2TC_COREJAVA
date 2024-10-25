66f29fc4a6f22d87b67bc155ee6fc6d64d000f49
LENOVO <LENOVO@-RB-27>
2024-10-25 16:43:23 +0530

    Second commit

---
+++ C2TC_VETRISELVI/src/com/tns/ifet/dayone/IdentifiersDemo.java
@@ -0,0 +1,12 @@
+package com.tns.ifet.dayone;
+
+public class IdentifiersDemo {
+	public static void main( String[] args){
+		int $number=20;
+		System.out.println("value of the number variable:"+ $number);
+		String studentName="Aniket";
+		System.out.println("value is:"+studentName);
+	}
+}
+
+
